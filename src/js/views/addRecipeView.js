4d26b0db46e5815acc627ec3cbc67cc7f77cb4f6
Zenister <hiimzenzen@gmail.com>
2025-03-27 15:51:26 +0800

    final addRecipeView edit

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -1,6 +1,5 @@
 import View from './View.js';
-// import icons from 'url:../../img/icons.svg';
-import icons from '../../img/icons.svg';
+import icons from 'url:../../img/icons.svg';
 import { MODAL_CLOSE_SEC } from '../config.js';
 
 class addRecipeView extends View {
